ec3d0a21cda5aeab7b8fd201290d9e4081c301ad
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2024-02-26 21:15:32 +0200

    create app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: NotesView(),
+      home: const NotesView(),
     );
   }
 }
