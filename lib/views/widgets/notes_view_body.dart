b2e80cb221b66badf158aaec1b5cbc87cf8feb5d
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2024-02-26 22:53:34 +0200

    create view item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -14,7 +15,55 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
         ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            contentPadding: EdgeInsetsDirectional.zero,
+            title: const Text(
+              'Flutter tips',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                    fontSize: 20, color: Colors.black.withOpacity(0.5)),
+              ),
+            ),
+            trailing: IconButton(
+                alignment: Alignment.topCenter,
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 22,
+                )),
+          ),
+          Text(
+            'May 21,2022',
+            style:
+                TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.5)),
+          ),
+        ]),
       ),
     );
   }
