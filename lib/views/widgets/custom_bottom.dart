cb739214c28ad87752fec0d67e7cbc1801bd88c7
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2024-03-11 16:35:42 +0200

    take input and validate data

--- lib/views/widgets/custom_bottom.dart
+++ lib/views/widgets/custom_bottom.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key});
-
+  const CustomBottom({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
-      child: const Center(
-        child: Text(
-          'add',
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
+        child: const Center(
+          child: Text(
+            'add',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
         ),
       ),
     );
